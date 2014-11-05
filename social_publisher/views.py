7b001e606bf68dffa5bbd9a8be83ebf4518283d1
susel <suselrd@gmail.com>
2014-11-05 16:06:13 -0500

    removing void-implemented methods from SocialPublication view

--- social_publisher/views.py
+++ social_publisher/views.py
@@ -8,16 +8,3 @@ class SocialPublication(FormView):
         context_data = super(SocialPublication, self).get_context_data(**kwargs)
         context_data['social_publication_form'] = PublicationForm(self.request.user)
         return context_data
-
-    def post(self, request, *args, **kwargs):
-        return super(SocialPublication, self).post(request, *args, **kwargs)
-
-    def get_form_class(self):
-        return super(SocialPublication, self).get_form_class()
-
-    def get_form(self, form_class):
-        return super(SocialPublication, self).get_form(form_class)
-
-    def get_form_kwargs(self):
-        return super(SocialPublication, self).get_form_kwargs()
-
